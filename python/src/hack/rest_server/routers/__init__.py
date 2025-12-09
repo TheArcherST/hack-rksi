577cb2858044af85448b9a7681bfd89d21231d75
Mihail Shapovalov <mihailsapovalov05@gmail.com>
2025-12-09 19:16:26 +0300

    Add tags to API Schema

--- python/src/hack/rest_server/routers/__init__.py
+++ python/src/hack/rest_server/routers/__init__.py
@@ -15,15 +15,16 @@ from . import (
 router = APIRouter()
 
 
-router.include_router(access.router)
+router.include_router(access.router, tags=["Access"])
+router.include_router(users.router, tags=["Admin panel"])
+router.include_router(events.router, tags=["Admin panel"])
+router.include_router(debug.router, tags=["Debug"])
+
 router.include_router(lead_sources.router, include_in_schema=False)
 router.include_router(operators.router, include_in_schema=False)
 router.include_router(lead_source_operators.router, include_in_schema=False)
 router.include_router(appeals.router, include_in_schema=False)
 router.include_router(inspect.router, include_in_schema=False)
-router.include_router(debug.router)
-router.include_router(users.router)
-router.include_router(events.router)
 
 
 __all__ = [
