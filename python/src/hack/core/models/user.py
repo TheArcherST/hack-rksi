ebe09903d19f8c7681ab40709fa54a98d90bdc2d
Mihail Shapovalov <mihailsapovalov05@gmail.com>
2025-12-09 17:30:59 +0300

    Add user role

--- python/src/hack/core/models/user.py
+++ python/src/hack/core/models/user.py
@@ -1,14 +1,22 @@
 from __future__ import annotations
 
+from enum import StrEnum
+
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base, CreatedAt
+
+
+class UserRoleEnum(StrEnum):
+    USER = "USER"
+    ADMINISTRATOR = "ADMINISTRATOR"
 
 
 class User(Base):
     __tablename__ = "user"
 
     id: Mapped[int] = mapped_column(primary_key=True)
+    role: Mapped[UserRoleEnum]
     username: Mapped[str] = mapped_column(unique=True)
     email: Mapped[str] = mapped_column(unique=True)
     full_name: Mapped[str] = mapped_column()
